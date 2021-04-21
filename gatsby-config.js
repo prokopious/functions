26615f835b9d6a57ad3e7182349ee0ae3cdc051f
tmhuyett <toddhuyett@gmail.com>
2021-04-20 22:12:19 -0400

    'd'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Squirrel Gorge`,
+    title: `Squirreltown`,
     description: `Stripe-powered squirrel shop`,
     author: `Brock McElroy <brxck.dev>`,
   },
